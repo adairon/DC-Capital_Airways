759e02bc69008358cb033d525e981b3c12ae089c
adairon <adairon@gmail.com>
2020-01-13 11:45:11 +0100

    création de fixtures pour Flight et City

--- readme.md
+++ readme.md
@@ -2,6 +2,7 @@ on défini l'accès à la bdd dans fichier .env
 - id = root
 - pw = root
 - port (sur MacOs) =  8889
+- ligne 28 : ```DATABASE_URL=mysql://root:root@127.0.0.1:8889/db_capital_airways?serverVersion=5.7```
 
 pr créer bdd : ```bin/console doctrine:database:create```
 
@@ -21,6 +22,11 @@ migration :
 ```bin/console make:migration```
 ```bin/console doctrine:migrations:migrate```
 
+installation des fixtures :
+- ```composer require --dev orm-fixtures```
 
+utilisation des fixtures (voir fichier AppFixtures.php):
+- création d'une fonction pour générer un numéro de vol aléatoire
+- création d'un tableau de villes pour alimenter les tables des entités
 
 
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\City;
+use App\Entity\Flight;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        // --alimenter la table City en utilisant l'entité City :
+        //On créé un tableau avec les villes :
+        $cities = ['Londres', 'Paris', 'Berlin', 'Lisbonne', 'Madrid','Bruxelles', 'Rome'];
+        $tabObjCity=[];
+        //on boucle pour chaque ville du tableau, on alimente un tableau d'objet avec les noms et on enregistre dans la BDD persist puis flush
+        foreach ($cities as $name){
+            $city = new City();
+            $city -> setName($name);
+            $tabObjCity[]=$city;
+            $manager -> persist($city);
+        }
+
+        $flight = new Flight();
+        $flight 
+            //on attribue le numéro de vol avec la fonction créée :
+            -> setNumber($this -> getFlightNumber())
+            // horaire uniquement en format hh:mm
+            -> setSchedule(\DateTime::createFromFormat('H:i', '08:00'))
+            -> setSeat(28)
+            -> setPrice(210)
+            -> setReduction(false)
+            -> setDeparture($tabObjCity[0])
+            -> setArrival($tabObjCity[4]);
+        $manager -> persist($flight);
+
+        $manager->flush();
+    }
+    
+    /**
+     * fonction pour créer le numéro de vol : 2 lettres et 4 chiffres aléatoires :
+     * @return string
+     */
+    public function getFlightNumber():string{
+        // créé un tableau avec toutes les lettres en majuscule de A à Z :
+        $lettres = range('A', 'Z'); 
+        // mélange les lettres aléatoirement :
+        shuffle($lettres); 
+        //extrait le 1er item du tableau : 
+        $lettre = array_shift($lettres);
+        //on recommence et on concatenne :
+        shuffle($lettres);
+        $lettre .= array_shift($lettres);
+        // un nombre sur 4 digit au hasard :
+        $nombre = mt_rand(1000,9999);
+        return $lettre.$nombre;
+ 
+
+
+
+
+    }
+}
